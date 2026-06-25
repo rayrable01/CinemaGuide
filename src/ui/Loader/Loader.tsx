ff3931a7defa79b6f59287c93cd3008d55c63015
Daniil Gnatiuk <gnatiukdanya@gmail.com>
2026-06-25 21:01:06 +0300

    feat: added vitest config

--- src/ui/Loader/Loader.tsx
+++ src/ui/Loader/Loader.tsx
@@ -3,7 +3,7 @@ import styles from "./Loader.module.css";
 
 const Loader: React.FC = () => {
   return (
-    <div className={styles.loader}>
+    <div className={styles.loader} data-testid="loader">
       <div className={styles.spinner}></div>
     </div>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
@@ -6,4 +6,9 @@ import svgr from "vite-plugin-svgr";
 export default defineConfig({
   base: "/CinemaGuide/",
   plugins: [react(), svgr()],
+  test: {
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
+    globals: true,
+  },
 });
